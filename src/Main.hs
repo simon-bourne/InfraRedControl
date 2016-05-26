6702225c2959c9079742177d7eeffe342e26a652
simon.bourne <simonbourne@enhedron.com>
2016-05-26 13:01:24 +0100

    Handle repeating keys

--- src/Main.hs
+++ src/Main.hs
@@ -12,9 +12,10 @@ import Data.Time.Clock
 import Data.List
 import System.Directory
 
-data Action = POWER | VOLUMEUP | VOLUMEDOWN | VIDEO4 | VIDEO5 deriving Show
+data ActionKey = POWER | VOLUMEUP | VOLUMEDOWN | VIDEO4 | VIDEO5 deriving (Show, Eq)
+data Action = Start ActionKey UTCTime | Stop
 
-type ActionChan = Chan (Action, UTCTime)
+type ActionChan = Chan Action
 
 keyPower, keyVolDown, keyVolUp, keySuper, keyAppleTV, keyPC :: Key
 keyPower = Key 38
@@ -28,26 +29,46 @@ released :: Key -> Event -> Bool
 released requestedKey (KeyEvent _ key Released) = key == requestedKey
 released _ _ = False
 
-sendAction :: ActionChan -> Maybe Action -> IO ()
+data IrSendAction = Send_Start | Send_Stop deriving Show
+
+irsend :: IrSendAction -> ActionKey -> IO ()
+irsend action key = do
+    (_exitCode , stdout, stderr) <-
+        readProcessWithExitCode "irsend" [show action, "sony-kdl40w2000", "KEY_" ++ show key] ""
+
+    when (stdout /= "") $ putStrLn stdout
+    when (stderr /= "") $ putStrLn stderr
+
+sendAction :: ActionChan -> Maybe ActionKey -> IO ()
 sendAction _ Nothing = return ()
 sendAction events (Just action) = do
     now <- getCurrentTime
-    writeChan events (action, now)
+    writeChan events $ Start action now
 
-runAction :: (Action, UTCTime) -> IO ()
-runAction (a, t) = do
+startAction :: ActionKey -> UTCTime -> IO (Maybe ActionKey)
+startAction key t = do
     now <- getCurrentTime
 
     if (diffUTCTime now t < 1) then do
-        (_exitCode , stdout, stderr) <-
-            readProcessWithExitCode "irsend" ["send_once", "sony-kdl40w2000", "KEY_" ++ show a] ""
-
-        when (stdout /= "") $ putStrLn stdout
-        when (stderr /= "") $ putStrLn stderr
-    else
+        irsend Send_Start key
+        return $ Just key
+    else do
         putStrLn "Discarding event older than 1 second"
+        return Nothing
 
-handleSuperModifiedKey :: Key -> Maybe Action
+runAction :: Maybe ActionKey -> Action -> IO (Maybe ActionKey)
+runAction (Just currentKey) Stop = do
+    irsend Send_Stop currentKey
+    return Nothing
+runAction (Just currentKey) (Start key t)
+    | currentKey == key = return $ Just key
+    | otherwise = do
+        irsend Send_Stop currentKey
+        startAction key t
+runAction Nothing (Start key t) = startAction key t
+runAction Nothing Stop = return Nothing
+
+handleSuperModifiedKey :: Key -> Maybe ActionKey
 handleSuperModifiedKey key
     | key == keyPower = Just POWER
     | key == keyPC = Just VIDEO4
@@ -67,7 +88,7 @@ handleSuper events f = do
 superPressed :: ActionChan -> Handle -> IO ()
 superPressed events f = void $ iterateUntil (released keySuper) $ handleSuper events f
 
-translateKey :: Key -> Maybe Action
+translateKey :: Key -> Maybe ActionKey
 translateKey key
     | key == keyVolDown = Just VOLUMEDOWN
     | key == keyVolUp = Just VOLUMEUP
@@ -81,6 +102,7 @@ translateDepressedKey events f key
 translateEvent :: ActionChan -> Handle -> Event -> IO ()
 translateEvent events f (KeyEvent _ key Depressed) = translateDepressedKey events f key
 translateEvent events _ (KeyEvent _ key Repeated) = sendAction events $ translateKey key
+translateEvent events _ (KeyEvent _ key Released) = writeChan events Stop
 translateEvent _ _ _ = return ()
 
 readEvent :: Handle -> IO Event
@@ -158,6 +180,9 @@ resetIguanaDevice device = do
 deviceDir :: String
 deviceDir = "/sys/bus/usb/devices"
 
+processEvent :: ActionChan -> Maybe ActionKey -> IO (Maybe ActionKey)
+processEvent events current = readChan events >>= runAction current
+
 main :: IO ()
 main = do
     stopService iguanaService
@@ -180,4 +205,4 @@ main = do
     events <- newChan
     void $ forkIO $ translateEvents events
 
-    forever (readChan events >>= runAction)
+    iterateM_ (processEvent events) Nothing
