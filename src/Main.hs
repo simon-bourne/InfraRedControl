2417767dfdd8f0596998a59e5d0898df6a455617
simon.bourne <simonbourne@enhedron.com>
2016-06-08 15:27:55 +0100

    TypedIO

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,17 @@
+{-# LANGUAGE DefaultSignatures #-}
+
 module Main (main) where
 
-import System.IO
+import TypedIO
+import System.IO (Handle)
 import System.Linux.Input.Event
 import Control.Monad
 import Control.Monad.Loops
 import Control.Monad.IfElse
-import System.Process
 import System.Exit
-import Control.Concurrent
-import Data.Time.Clock
+import Control.Concurrent (Chan)
+import Data.Time.Clock (UTCTime, diffUTCTime)
 import Data.List
-import System.Directory
 
 data ActionKey = POWER | VOLUMEUP | VOLUMEDOWN | VIDEO4 | VIDEO5 deriving (Show, Eq)
 data Action = Start ActionKey UTCTime | Stop
@@ -31,22 +32,34 @@ released _ _ = False
 
 data IrSendAction = Send_Once | Send_Start | Send_Stop deriving Show
 
-irsend :: IrSendAction -> ActionKey -> IO ()
-irsend action key = do
-    (_exitCode , output, errorOutput) <-
-        readProcessWithExitCode "irsend" [show action, "sony-kdl40w2000", "KEY_" ++ show key] ""
-    logOutput output
-    logOutput errorOutput
-      where
-        logOutput o = when (o /= "") $ putStrLn o
+class Monad m => IrSender m where
+    irsend :: IrSendAction -> ActionKey -> m ()
+    default irsend :: (RunProcess m, StdoutWriter m) => IrSendAction -> ActionKey -> m ()
+    irsend action key = do
+        (_exitCode , output, errorOutput) <-
+            readProcessWithExitCode "irsend" [show action, "sony-kdl40w2000", "KEY_" ++ show key] ""
+        logOutput output
+        logOutput errorOutput
+          where
+            logOutput o = when (o /= "") $ writeStdoutLn o
 
-sendAction :: ActionChan -> Maybe ActionKey -> IO ()
-sendAction _ Nothing = return ()
-sendAction events (Just action) = do
-    now <- getCurrentTime
-    writeChan events $ Start action now
+instance IrSender IO
 
-irstart :: ActionKey -> IO (Maybe ActionKey)
+class Monad m => ActionSender m where
+    sendAction :: ActionChan -> Maybe ActionKey -> m ()
+    default sendAction :: (CurrentTime m, Producer m) => ActionChan -> Maybe ActionKey -> m ()
+    sendAction _ Nothing = return ()
+    sendAction events (Just action) = do
+        now <- getCurrentTime
+        writeChan events $ Start action now
+
+    sendStop :: ActionChan -> m ()
+    default sendStop :: Producer m => ActionChan -> m ()
+    sendStop events = writeChan events Stop
+
+instance ActionSender IO
+
+irstart :: IrSender m => ActionKey -> m (Maybe ActionKey)
 irstart key
     | key `elem` [VOLUMEUP, VOLUMEDOWN] = do
         irsend Send_Start key
@@ -55,17 +68,17 @@ irstart key
         irsend Send_Once key
         return Nothing
 
-startAction :: ActionKey -> UTCTime -> IO (Maybe ActionKey)
+startAction :: (CurrentTime m, IrSender m, StdoutWriter m) => ActionKey -> UTCTime -> m (Maybe ActionKey)
 startAction key t = do
     now <- getCurrentTime
 
     if (diffUTCTime now t < 1) then
         irstart key
     else do
-        putStrLn "Discarding event older than 1 second"
+        writeStdoutLn "Discarding event older than 1 second"
         return Nothing
 
-runAction :: Maybe ActionKey -> Action -> IO (Maybe ActionKey)
+runAction :: (CurrentTime m, IrSender m, StdoutWriter m) => Maybe ActionKey -> Action -> m (Maybe ActionKey)
 runAction (Just currentKey) Stop = do
     irsend Send_Stop currentKey
     return Nothing
@@ -84,7 +97,7 @@ handleSuperModifiedKey key
     | key == keyAppleTV = Just VIDEO5
     | otherwise = Nothing
 
-handleSuper :: ActionChan -> Handle -> IO Event
+handleSuper :: (EvdevReader m, ActionSender m) => ActionChan -> Handle -> m Event
 handleSuper events f = do
     e <- readEvent f
     sendAction events $ case e of
@@ -94,7 +107,7 @@ handleSuper events f = do
 
     return e
 
-superPressed :: ActionChan -> Handle -> IO ()
+superPressed :: (EvdevReader m, ActionSender m) => ActionChan -> Handle -> m ()
 superPressed events f = void $ iterateUntil (released keySuper) $ handleSuper events f
 
 translateKey :: Key -> Maybe ActionKey
@@ -103,21 +116,18 @@ translateKey key
     | key == keyVolUp = Just VOLUMEUP
     | otherwise = Nothing
 
-translateDepressedKey :: ActionChan -> Handle -> Key -> IO ()
+translateDepressedKey :: (EvdevReader m, ActionSender m) => ActionChan -> Handle -> Key -> m ()
 translateDepressedKey events f key
     | key == keySuper = superPressed events f
     | otherwise = sendAction events $ translateKey key
 
-translateEvent :: ActionChan -> Handle -> Event -> IO ()
+translateEvent :: (EvdevReader m, ActionSender m) => ActionChan -> Handle -> Event -> m ()
 translateEvent events f (KeyEvent _ key Depressed) = translateDepressedKey events f key
 translateEvent events _ (KeyEvent _ key Repeated) = sendAction events $ translateKey key
-translateEvent events _ (KeyEvent _ _ Released) = writeChan events Stop
+translateEvent events _ (KeyEvent _ _ Released) = sendStop events
 translateEvent _ _ _ = return ()
 
-readEvent :: Handle -> IO Event
-readEvent f = untilJust $ hReadEvent f
-
-readEvents :: ActionChan -> Handle -> IO ()
+readEvents :: (EvdevReader m, ActionSender m) => ActionChan -> Handle -> m ()
 readEvents events f = forever (readEvent f >>= translateEvent events f)
 
 exitedSuccessfully :: ExitCode -> Bool
@@ -127,23 +137,23 @@ exitedSuccessfully _ = False
 seconds :: Int -> Int
 seconds n = n * (10 ^ (6 :: Int))
 
-untilSuccessful :: String -> [String] -> IO ()
+untilSuccessful :: (Sleeper m, StdoutWriter m, RunProcess m) => String -> [String] -> m ()
 untilSuccessful command args = void $ iterateUntil exitedSuccessfully $ do
-    putStr "Waiting for "
-    putStrLn command
-    (exitCode, _, _) <- readProcessWithExitCode command args ""
-    threadDelay $ seconds 1
+    writeStdout "Waiting for "
+    writeStdoutLn command
+    exitCode <- runProcessWithExitCode command args
+    sleep $ seconds 1
     return exitCode
 
-stopService :: String -> IO ()
-stopService name = void $ readProcessWithExitCode "service" [name, "stop"] ""
+stopService :: RunProcess m => String -> m ()
+stopService name = void $ runProcessWithExitCode "service" [name, "stop"]
 
-startService :: String -> IO ()
-startService name = callProcess "service" [name, "start"]
+startService :: RunProcess m => String -> m ()
+startService name = runProcessOrFail "service" [name, "start"]
 
-translateEvents :: ActionChan -> IO ()
+translateEvents :: (EvdevReader m, ActionSender m, FileReader m) => ActionChan -> m ()
 translateEvents events =
-    withFile "/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-mouse" ReadMode $ readEvents events
+    withReadOnlyFile "/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-mouse" $ readEvents events
 
 -- Disable iguanaIR and lirc services with:
 --  sudo update-rc.d iguanaIR disable
@@ -157,61 +167,78 @@ iguanaService, lircService :: String
 iguanaService = "iguanaIR"
 lircService = "lirc"
 
-filesAllExist :: [String] -> IO Bool
+filesAllExist :: DirectoryReader m => [String] -> m Bool
 filesAllExist files = andM (doesFileExist <$> files)
 
 isIguanaDevice :: String -> String -> Bool
 isIguanaDevice vendorId productId =
     vendorId == iguanaVendorId && productId == iguanaProductId
 
-resetUSBDevice :: String -> IO ()
+resetUSBDevice :: (StdoutWriter m, FileWriter m, Sleeper m) => String -> m ()
 resetUSBDevice devicePath = do
     let authorizedPath = devicePath ++ "/authorized"
 
-    putStrLn "Resetting IR transceiver"
-    writeFile authorizedPath "0\n"
-    threadDelay $ seconds 1
-    writeFile authorizedPath "1\n"
-    threadDelay $ seconds 1
+    writeStdoutLn "Resetting IR transceiver"
+    writeToFile authorizedPath "0\n"
+    sleep $ seconds 1
+    writeToFile authorizedPath "1\n"
+    sleep $ seconds 1
 
-resetIguanaDevice :: String -> IO ()
-resetIguanaDevice device = do
+resetIfIguanaDevice :: (StdoutWriter m, FileReader m, FileWriter m, Sleeper m, DirectoryReader m) => String -> m ()
+resetIfIguanaDevice device = do
     let devicePath = deviceDir ++ "/" ++ device
     let vendorIdFile = devicePath ++ "/idVendor"
     let productIdFile = devicePath ++ "/idProduct"
 
     whenM (filesAllExist [vendorIdFile, productIdFile]) $ do
-        vendorId <- readFile vendorIdFile
-        productId <- readFile productIdFile
+        vendorId <- readFromFile vendorIdFile
+        productId <- readFromFile productIdFile
 
         when (isIguanaDevice vendorId productId) $ resetUSBDevice devicePath
 
 deviceDir :: String
 deviceDir = "/sys/bus/usb/devices"
 
-processEvent :: ActionChan -> Maybe ActionKey -> IO (Maybe ActionKey)
+processEvent :: (Consumer m, CurrentTime m, IrSender m, StdoutWriter m) => ActionChan -> Maybe ActionKey -> m (Maybe ActionKey)
 processEvent events current = readChan events >>= runAction current
 
-main :: IO ()
-main = do
-    stopService iguanaService
-    stopService lircService
+stopServices :: RunProcess m => m ()
+stopServices = stopService iguanaService >> stopService lircService
 
+resetIguanaDevices :: (StdoutWriter m, Sleeper m, DirectoryReader m, FileWriter m, FileReader m) => m ()
+resetIguanaDevices = do
     usbDevices <- (\\ [".", ".."]) <$> getDirectoryContents deviceDir
 
-    mapM_ resetIguanaDevice usbDevices
+    mapM_ resetIfIguanaDevice usbDevices
 
+startServices :: (StdoutWriter m, RunProcess m, Sleeper m) => m ()
+startServices = do
     startService iguanaService
     untilSuccessful "igclient" ["--get-version"]
 
     startService lircService
     untilSuccessful "irsend" ["set_transmitters", "1"]
 
-    threadDelay $ seconds 1
+    sleep $ seconds 1
 
-    putStrLn "Running"
+    writeStdoutLn "Running"
 
+runThreads :: (Controller m,
+               FileReader m,
+               EvdevReader m,
+               ActionSender m,
+               StdoutWriter m,
+               IrSender m,
+               CurrentTime m) => m ()
+runThreads = do
     events <- newChan
-    void $ forkIO $ translateEvents events
+    void $ fork $ translateEvents events
 
     iterateM_ (processEvent events) Nothing
+
+main :: IO ()
+main = do
+    stopServices
+    resetIguanaDevices
+    startServices
+    runThreads
---
+++ src/TypedIO.hs
@@ -0,0 +1,104 @@
+module TypedIO (
+    StdoutWriter(..),
+    Consumer(..),
+    Producer(..),
+    Controller(..),
+    Sleeper(..),
+    RunProcess(..),
+    CurrentTime(..),
+    FileReader(..),
+    FileWriter(..),
+    DirectoryReader(..),
+    EvdevReader(..)
+) where
+
+import qualified Control.Concurrent as Concurrent
+import Control.Concurrent (Chan, ThreadId)
+import Control.Monad.Loops
+import qualified Data.Time.Clock as Clock
+import Data.Time.Clock (UTCTime)
+import qualified System.Directory as Directory
+import System.Linux.Input.Event
+import qualified System.Process as Process
+import System.Exit
+import System.IO (Handle, IOMode(..), withFile)
+
+class Monad m => StdoutWriter m where
+    writeStdout :: String -> m ()
+    writeStdoutLn :: String -> m ()
+
+instance StdoutWriter IO where
+    writeStdout = putStr
+    writeStdoutLn = putStrLn
+
+class Monad m => Consumer m where
+    readChan :: Chan a -> m a
+
+instance Consumer IO where
+    readChan = Concurrent.readChan
+
+class Monad m => Producer m where
+    writeChan :: Chan a -> a -> m ()
+
+instance Producer IO where
+    writeChan = Concurrent.writeChan
+
+class (Producer m, Consumer m) => Controller m where
+    newChan :: m (Chan a)
+    fork :: m () -> m ThreadId
+
+instance Controller IO where
+    newChan = Concurrent.newChan
+    fork = Concurrent.forkIO
+
+class Monad m => Sleeper m where
+    sleep :: Int -> m ()
+
+instance Sleeper IO where
+    sleep = Concurrent.threadDelay
+
+class Monad m => RunProcess m where
+    readProcessWithExitCode :: String -> [String] -> String -> m (ExitCode, String, String)
+    runProcessWithExitCode :: String -> [String] -> m ExitCode
+    runProcessOrFail :: String -> [String] -> m ()
+
+instance RunProcess IO where
+    readProcessWithExitCode = Process.readProcessWithExitCode
+    runProcessWithExitCode command args = do
+        (exitCode, _, _) <- Process.readProcessWithExitCode command args ""
+        return exitCode
+    runProcessOrFail = Process.callProcess
+
+class Monad m => CurrentTime m where
+    getCurrentTime :: m UTCTime
+
+instance CurrentTime IO where
+    getCurrentTime = Clock.getCurrentTime
+
+class Monad m => FileReader m where
+    readFromFile :: FilePath -> m String
+    withReadOnlyFile :: FilePath -> (Handle -> m a) -> m a
+
+instance FileReader IO where
+    readFromFile = readFile
+    withReadOnlyFile path = withFile path ReadMode
+
+class Monad m => FileWriter m where
+    writeToFile :: FilePath -> String -> m ()
+
+instance FileWriter IO where
+    writeToFile = writeFile
+
+class Monad m => DirectoryReader m where
+    doesFileExist :: FilePath -> m Bool
+    getDirectoryContents :: FilePath -> m [FilePath]
+
+instance DirectoryReader IO where
+    doesFileExist = Directory.doesFileExist
+    getDirectoryContents = Directory.getDirectoryContents
+
+class Monad m => EvdevReader m where
+    readEvent :: Handle -> m Event
+
+instance EvdevReader IO where
+    readEvent f = untilJust $ hReadEvent f
